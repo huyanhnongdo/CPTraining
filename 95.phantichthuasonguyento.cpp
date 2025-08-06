900ff81fdc3f82fb94475ebbd5849cc74cacb677
Nông Đỗ Huy Anh <116568402+huyanhnongdo@users.noreply.github.com>
2025-08-06 21:54:20 +0700

    Create 95.phantichthuasonguyento.cpp

---
+++ 95.phantichthuasonguyento.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define LIMIT int(1e6)+5
+using namespace std;
+
+bitset<LIMIT> is_prime;
+vector<int> primes;
+void sieve(){
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+    for(int i = 2;i * i < LIMIT;i++){
+        if(is_prime[i]){
+            for(int j = i * i; j < LIMIT; j+= i){
+                is_prime[j] = 0;
+            }
+        }
+    }
+    for(int i = 2;i <= LIMIT;i++){
+        if(is_prime[i])
+            primes.push_back(i);
+    }
+}
+
+void pt(int n){
+    int i = 2;
+    int s,m = 0;
+    while(n>1){
+        while(n%i==0){
+            s=i;
+            m+=1;
+            n/=i;
+        }
+        if(s>0 && m>0) cout << s << ' ' << m << ' ';
+        i++;m=0;
+    } cout << '\n';
+}
+
+// 2 3 5 7
+void solve(){
+    int T;cin >> T;
+    while(T--){
+        int x;cin>>x;
+        pt(x);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    solve();
+    return 0;
+}
