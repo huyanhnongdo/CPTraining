36a6ec450aa939c1ab6ff0cb6a47a281bd9dd697
Nông Đỗ Huy Anh <116568402+huyanhnongdo@users.noreply.github.com>
2025-08-06 22:03:01 +0700

    a

---
+++ 96.phantichthuasonguyento.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define LIMIT int(1e6)+5
+using namespace std;
+
+bitset<LIMIT> is_prime;
+vector<int> primes;
+void sieve(){
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+    for(int i = 2;i * i < LIMIT;i++){
+        if(is_prime[i]){
+            for(int j = i * i; j < LIMIT; j+= i){
+                is_prime[j] = 0;
+            }
+        }
+    }
+    for(int i = 2;i <= LIMIT;i++){
+        if(is_prime[i])
+            primes.push_back(i);
+    }
+}
+
+void pt(int n, int k){
+    int j = 1;
+    int i = 2;
+    int s,m = 0;
+    while(n>1){
+        while(n%i==0){
+            s=i;
+            m+=1;
+            n/=i;
+            if(j==k){
+                cout << s << '\n';return;
+            }
+            j+=1;
+        }
+        i++;m=0;
+    } cout << -1 << '\n';
+}
+
+// 2 3 5 7
+void solve(){
+    int T;cin >> T;
+    while(T--){
+        int n,k;
+        cin >> n >> k;
+        pt(n,k);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    solve();
+    return 0;
+}
